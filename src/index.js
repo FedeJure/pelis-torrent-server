8eeb881d0dcae867ca47da5cd174eb80e48ab9c7
Federico Jure <fjure@atixlabs.com>
2020-06-20 20:19:35 -0300

    agrego proxy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const path = require('path')
 const AutoLoad = require('fastify-autoload')
 const TorrentSearchApi = require('torrent-search-api');
+const proxy = require('fastify-http-proxy');
 
 
 module.exports = function (fastify, opts, next) {
@@ -11,6 +12,16 @@ module.exports = function (fastify, opts, next) {
   // through your application
   fastify.torrent = TorrentSearchApi;
   fastify.torrent.enablePublicProviders();
+
+  fastify.register(proxy, {
+    upstream: 'https://api.themoviedb.org/3/search/movie',
+    prefix: '/tmdb'
+  });
+
+  fastify.register(proxy, {
+    upstream: 'https://yts.mx/api/v2/list_movies.json',
+    prefix: '/yts'
+  });
 
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
