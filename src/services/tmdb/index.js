78f303b282c5e0000a2fe8b653f487db8979a79e
Federico Jure <fedejure@gmail.com>
2020-06-21 01:40:50 -0300

    fix

--- src/services/tmdb/index.js
+++ src/services/tmdb/index.js
@@ -2,8 +2,8 @@
 /**parameters allowed: name, category... */
 module.exports = async function (fastify, opts) {
   fastify.get('/tmdb/search', async function (request, reply) {
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=133f4d8b4fed128b27fa0bb407956c56&language=${request.query.language}&page=1&include_adult=false&query=${request.query.query}`)
-    .then(res => reply.status(200).send(res))
+    fastify.axios.get(`https://api.themoviedb.org/3/search/movie?api_key=133f4d8b4fed128b27fa0bb407956c56&language=${request.query.language}&page=1&include_adult=false&query=${request.query.query}`)
+    .then(res => reply.status(200).send(res.data))
     .catch(err => reply.status(400).send(err));
   });
 }
