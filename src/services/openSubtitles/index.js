bfc2578cc36b2f953f1ff45a361b00bb9870da9c
Federico Jure <fjure@atixlabs.com>
2020-07-12 23:59:03 -0300

    add opensubtitles api

---
+++ src/services/openSubtitles/index.js
@@ -0,0 +1,11 @@
+const OpenSubtitles = require("opensubtitles-api");
+
+'use strict'
+/**parameters allowed: name, category... */
+module.exports = async function (fastify, opts) {
+  const openSubtitles = new OpenSubtitles('UserAgent');
+  fastify.get('/openSubtitles/search', async function (request, reply) {
+    const subtitles = await openSubtitles.search({imdbid: request.query.imdbid, sublanguageid: "spa,eng,por", extensions: ['vtt']});
+    reply.status(200).send(subtitles);
+  });
+}
