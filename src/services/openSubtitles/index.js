81694d0d8caf6214097339a343e8c6b190d5e4af
Federico Jure <fedejure@gmail.com>
2020-07-15 01:45:31 -0300

    add subtitles

--- src/services/openSubtitles/index.js
+++ src/services/openSubtitles/index.js
@@ -1,11 +1,50 @@
 const OpenSubtitles = require("opensubtitles-api");
-
+const availableLanguages = ['en','es'];
 'use strict'
 /**parameters allowed: name, category... */
 module.exports = async function (fastify, opts) {
-  const openSubtitles = new OpenSubtitles('UserAgent');
-  fastify.get('/openSubtitles/search', async function (request, reply) {
-    const subtitles = await openSubtitles.search({imdbid: request.query.imdbid, sublanguageid: 'en,es'});
-    reply.status(200).send(subtitles);
+  const openSubtitles = new OpenSubtitles({
+    useragent:'UserAgent',
+    username: 'fedejure1',
+    password: 'fedejure11',
+    ssl: true
   });
+  var session = {logged: false};
+  openSubtitles.api.LogIn('fedejure1', 'fedejure11', 'es', 'UserAgent')
+    .then(res => {
+        session.token = res.token;
+        session.user = res.data;
+    })
+    .catch(err => {
+        console.log(err);
+    });
+  fastify.get('/openSubtitles/search', async function (request, reply) {
+    // const subtitles = await openSubtitles.search({imdbid: request.query.imdbid, sublanguageid: 'en,es'});
+    const subtitles = await openSubtitles.api.SearchSubtitles(session.token,[
+      {"imdbid": request.query.imdbid.slice(2,request.query.imdbid.length)},
+    ]);
+    response = {};
+
+    if (subtitles.data && subtitles.data.filter)
+      subtitles.data.forEach(sub => {
+        if (!availableLanguages.includes(sub.ISO639)) return;
+        if (!response[sub.SubLanguageID]) response[sub.SubLanguageID] = [toSubtitleObject(sub)];
+        else response[sub.SubLanguageID].push(toSubtitleObject(sub));
+      });
+    reply.status(200).send(response);
+  });
+}
+
+const toSubtitleObject = sub => {
+  return ({
+    score: sub.SubRating,
+    url: getVttUrl(sub.SubDownloadLink),
+    languageCode: sub.SubLanguageID,
+    languageName: sub.LanguageName,
+    imdbid: sub.IDMovieImdb
+  });
+}
+
+const getVttUrl = url => {
+  return url.replace("/filead", "/subformat-vtt/filead").replace(".gz", "");
 }
